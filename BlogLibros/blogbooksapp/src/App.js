72043aa2bac02a5bcd9b5a4e058f8da5e0e8c124
Leandro Magallanes <thesharkcommunity@gmail.com>
2022-08-30 06:10:39 +0200

    arreglar rutas

--- BlogLibros/blogbooksapp/src/App.js
+++ BlogLibros/blogbooksapp/src/App.js
@@ -10,16 +10,12 @@ const App = () => {
   return (
     <>
         <Routes>
-            <Route path="/home" element={<HomePage/>}/>
+            <Route path="/" element={<HomePage/>}/>
             <Route path="about" element={<AboutPage/>}/>
             <Route path="error" element={<Error404Page/>}/>
             <Route path="contacto" element={<ContactPage/>}/>
             <Route path="proyectos" element={<ProjectsPage/>}/>
-<<<<<<< HEAD
-            <Route path="/*" element={<Navigate to="/home"/>}/>
-=======
-            <Route path="/*" element={<Navigate to="/HomePage"/>}/>
->>>>>>> 23928e927527441f6d9939595f5712697913aa64
+            <Route path="/*" element={<Navigate to="/"/>}/>
         </Routes>
     </>
   )
